b6f658c41749006b858bfaf8f24b746907b946d8
sac6inarya <sachinkarya333@gmail.com>
2023-08-05 21:25:15 +0530

    add task code

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-console.log("Logging!");
+const posts = [{ title: 'POST1' }];
+
+
+function updateLastUserActivityTime() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let lastActivity = new Date();
+            resolve(lastActivity);
+        }, 1000);
+    })
+}
+
+function createPost(val) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.push(val);
+            resolve()
+        }, 3000)
+    })
+}
+
+
+function deletePost() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.pop();
+            resolve();
+        }, 1000)
+    })
+}
+
+function log() {
+    posts.forEach((item) => {
+        console.log(item.title);
+    })
+}
+
+createPost({ title: "POST2" })
+    .then(() => {
+        let uTime = updateLastUserActivityTime();
+        return uTime;
+    }).then((val) => {
+        log();
+        console.log(val);
+    })
+deletePost().then(() => {
+    log()
+})
