a78116d9546171951c56111ca0699d3a7b4c3405
sac6inarya <sachinkarya333@gmail.com>
2023-08-11 02:15:43 +0530

    delete feature

--- index.js
+++ index.js
@@ -1,74 +1,101 @@
-// let storedItems = JSON.parse(localStorage.getItem("items")) || [];
-const loadItemsData = ()=>{
+let items = document.getElementById("list-container");
+let form = document.getElementById("form");
+
+const loadItemsData = () => {
     axios.get("https://crudcrud.com/api/a2b8a9f39da84a1d999c0b1270e8f36d/posteddata")
-    .then((storedItems)=>{
+    .then((storedItems) => {
+        items.innerHTML = ""; // Clear existing items before loading new ones
         storedItems.data.forEach(element => {
-            let li = document.createElement("li");
-            li.innerHTML = `
-            <span>Name: ${element.name}</span>
-            <span>Email: ${element.email}</span>
-            <span>Phone: ${element.phone}</span>
-            `
-            items.appendChild(li);
+            createListItem(element);
         });
     })
-    .catch((err)=>{
+    .catch((err) => {
         console.error(err);
     })
 }
 
-
-
-// document.addEventListener("DOMContentLoaded",()=>{
-//     if(storedItems){
-//         storedItems.forEach(element => {
-//             let li = document.createElement("li");
-//             li.innerHTML = `
-//             <span> Name: ${element.name}</span>
-//             <span> Email: ${element.email}</span>
-//             <span> Phone: ${element.phone}</span>
-//             `
-//             items.appendChild(li);
-//         });
-//     }
-// })
-
-
 document.addEventListener("DOMContentLoaded", loadItemsData);
 
-let items = document.getElementById("list-container");
-let form = document.getElementById("form");
+form.addEventListener("submit", addItem);
 
-form.addEventListener("submit",addItem);
-
-function addItem(e){
-    e.preventDefault()
+function addItem(e) {
+    e.preventDefault();
     let nameValue = document.getElementById("name").value;
     let emailValue = document.getElementById("email").value;
     let phoneValue = document.getElementById("phone").value;
 
-    let li = document.createElement("li");
-    li.innerHTML = `
-    <span> Name: ${nameValue}</span>
-    <span> Email: ${emailValue}</span>
-    <span> Phone: ${phoneValue}</span>
-    `
     let object = {
-        name:nameValue,
-        email:emailValue,
-        phone:phoneValue
+        name: nameValue,
+        email: emailValue,
+        phone: phoneValue
     }
 
-    // storedItems.push(object);
-    // localStorage.setItem("items", JSON.stringify(storedItems));
-
     axios.post("https://crudcrud.com/api/a2b8a9f39da84a1d999c0b1270e8f36d/posteddata", object)
-    .then((res)=>{
-        items.appendChild(li);
+    .then((res) => {
+        createListItem(res.data);
     })
-    .catch((err)=>{
-        document.body.innerHTML = document.body.innerHTML + "<h2>Something went wrong";
+    .catch((err) => {
         console.error(err);
-    })
+    });
+
     form.reset();
+}
+
+function createListItem(element) {
+    let li = document.createElement("li");
+
+    let deleteButton = document.createElement("button");
+    let editButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    editButton.textContent = "Edit";
+    deleteButton.addEventListener("click", (e) => deleteItem(e, element._id));
+    editButton.addEventListener("click", () => editItem(element._id));
+
+    li.innerHTML = `
+    <span>Name: ${element.name}</span>
+    <span>Email: ${element.email}</span>
+    <span>Phone: ${element.phone}</span>
+    `
+    li.appendChild(deleteButton);
+    li.appendChild(editButton);
+    items.appendChild(li);
+}
+
+function deleteItem(e, id) {
+    axios.delete(`https://crudcrud.com/api/a2b8a9f39da84a1d999c0b1270e8f36d/posteddata/${id}`)
+    .then((res) => {
+        let targetEle = e.target.parentElement;
+        items.removeChild(targetEle);
+        console.log("Deleted");
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+}
+
+
+function editItem(id) {
+    axios.get(`https://crudcrud.com/api/a2b8a9f39da84a1d999c0b1270e8f36d/posteddata/${id}`)
+    .then((res) => {
+        let currentItem = res.data;
+        let newName = prompt("Edit name", currentItem.name); 
+        let newEmail = prompt("Edit email", currentItem.email); 
+        let newPhone = prompt("Edit phone", currentItem.phone); 
+
+        axios.put(`https://crudcrud.com/api/a2b8a9f39da84a1d999c0b1270e8f36d/posteddata/${id}`, {
+            name: newName,
+            email: newEmail,
+            phone: newPhone
+        })
+        .then((res) => {
+            console.log("Updated");
+            loadItemsData(); 
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 }
