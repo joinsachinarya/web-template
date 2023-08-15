23e11c57daf0364bfde7b185c4a39ee60c87d582
sac6inarya <sachinkarya333@gmail.com>
2023-08-15 11:27:27 +0530

    cleanup

--- index.js
+++ index.js
@@ -1,90 +1,70 @@
-// // // this with regular function
-// // class Student{
-// //     constructor(name, age, marks){
-// //         this.name = name;
-// //         this.age = age;
-// //         this.marks = marks;
-// //     }
-// //     isReadyForPlacement(minAge){
-// //         console.log("outer function",this);
-// //         return function isMarksGood(minMarks){
-// //             if(this.age>minAge && this.marks>minMarks){
-// //                 console.log("inner function", this);
-// //                 console.log(`${this.name} is ready for placement`);
-// //             }else{
-// //                 console.log(`${this.name} is not ready for placement`);
-// //             }
-// //         }
-// //     }
-// // }
+// this with regular function
+class Student {
+  constructor(name, age, marks) {
+    this.name = name;
+    this.age = age;
+    this.marks = marks;
+  }
+  isReadyForPlacement(minAge) {
+    console.log("outer function", this);
+    return function isMarksGood(minMarks) {
+      if (this.age > minAge && this.marks > minMarks) {
+        console.log("inner function", this);
+        console.log(`${this.name} is ready for placement`);
+      } else {
+        console.log(`${this.name} is not ready for placement`);
+      }
+    };
+  }
+}
 
 // // this with arrow function
-// class Student{
-//     constructor(name, age, marks){
-//         this.name = name;
-//         this.age  = age;
-//         this.marks = marks;
-//     }
-//     isReadyForPlacement(minAge){
-//         console.log("outer function", this);
-//         return (minMarks)=>{
-//             console.log("inner function", this);
-//             if(this.age>minAge && this.marks>minMarks){
-//                 console.log(`${this.name} is ready for placement`);
-//             }else{
-//                 console.log(`${this.name} is not ready for placement`);
-//             }
+// class Student {
+//   constructor(name, age, marks) {
+//     this.name = name;
+//     this.age = age;
+//     this.marks = marks;
+//   }
+//   isReadyForPlacement(minAge) {
+//     console.log("outer function", this);
+//     return (minMarks) => {
+//       console.log("inner function", this);
+//       if (this.age > minAge && this.marks > minMarks) {
+//         console.log(`${this.name} is ready for placement`);
+//       } else {
+//         console.log(`${this.name} is not ready for placement`);
+//       }
+//     };
+//   }
+// }
+
+// // this with arrow function :: this does not create it's own context instead it find the value of this from it's lexical parent
+// class Student {
+//   constructor(name, age, marks) {
+//     this.name = name;
+//     this.age = age;
+//     this.marks = marks;
+//   }
+//   isReadyForPlacement(minAge) {
+//     console.log("outer function", this);
+//     return (minMarks) => {
+//       console.log("inner function", this);
+//       return (maxGap) => {
+//         console.log("inner inner function ", this);
+//         if (this.age > minAge && this.marks > minMarks && maxGap <= 2) {
+//           console.log(`${this.name} is ready for placement`);
+//         } else {
+//           console.log(`${this.name} is not ready for placement`);
 //         }
-//     }
+//       };
+//     };
+//   }
 // }
 
-// // // this with arrow function :: this does not create it's own context instead it find the value of this from it's lexical parent
-// // class Student{
-// //     constructor(name, age, marks){
-// //         this.name = name;
-// //         this.age  = age;
-// //         this.marks = marks;
-// //     }
-// //     isReadyForPlacement(minAge){
-// //         console.log("outer function", this);
-// //         return (minMarks)=>{
-// //             console.log("inner function", this);
-// //             return (maxGap)=>{
-// //                 console.log("inner inner function ", this);
-// //                 if(this.age>minAge && this.marks>minMarks && maxGap<=2){
-// //                     console.log(`${this.name} is ready for placement`);
-// //                 }else{
-// //                     console.log(`${this.name} is not ready for placement`);
-// //                 }
-// //             }
-// //         }
-// //     }
-// // }
+const sachin = new Student("sachin", 20, 50);
+sachin.isReadyForPlacement(16)(33);
 
-// const sachin = new Student("sachin", 20, 50);
-// sachin.isReadyForPlacement(16)(33);
-
-// function myFun(){
-//     console.log(arguments);
-// }
-// myFun(1,2,3)
-
-// (()=>{
-//     console.log(arguments);
-// })()
-
-function sum(a, b) {
-  return (a += b);
+function myFun() {
+  console.log(arguments);
 }
-
-console.log(sum(6, 5)); //11
-console.log(sum(6, 5)); //11
-console.log(sum(6, 5)); //11
-
-let a = 6;
-function sum2(b) {
-  return (a += b);
-}
-console.log(sum2(5)); //11
-console.log(sum2(5)); //16
-console.log(sum2(5)); //21
+myFun(1, 2, 3);
