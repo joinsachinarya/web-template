7431db550357a9de746401ace84f5cd4ae64407e
sac6inarya <sachinkarya333@gmail.com>
2023-08-15 18:46:50 +0530

    using req.url mehtod

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const http = require("http");
+
 const server = http.createServer((req, res) => {
-  console.log("hey seachin from server");
+  let url = req.url;
+  if (url === "/home") {
+    // res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("Welcome ");
+    res.end("home");
+  } else if (url === "/about") {
+    // res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("Welcome ");
+    res.end("to about us page");
+  } else if (url === "/node") {
+    // res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("Welcome ");
+    res.end("to my node js project");
+  }
 });
-server.listen(4000);
+
+server.listen(4000, () => {
+  console.log("Server is listening on port 4000");
+});
