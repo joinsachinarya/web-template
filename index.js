ffc6ec8b5d1adbeebe445d6020adc897386676df
sac6inarya <sachinkarya333@gmail.com>
2023-08-15 20:48:45 +0530

    sending data from client and recieving from server

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   let url = req.url;
-  if (url === "/home") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("Welcome ");
-    res.end("<h1>home</h1>");
-  } else if (url === "/about") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("Welcome ");
-    res.end("<h1>to about us page</h1>");
-  } else if (url === "/node") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("Welcome ");
-    res.end("<h1>to my node js project</h1>");
-    // res.write("error");
+  let method = req.method;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'> <input type='text' name='myMessage' placeholder='message'> <button type='submit'>Send</button> </form></body>"
+    );
+    res.write("</html>");
+    return res.end();
   }
-  // process.exit();
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunks) => {
+      console.log("chunks", chunks);
+      body.push(chunks);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>App</title></head>");
+  res.write("<body><h2>Welcome to my NodeJS app.</h2></body>");
+  res.write("</html>");
+  return res.end();
 });
 
 server.listen(4000, () => {
