850d2ec6e1ccd51b62ee4e3b9c5e999ef7e6dd25
sac6inarya <sachinkarya333@gmail.com>
2023-08-15 11:28:52 +0530

    init

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Logging!");
+// (()=>{
+//     console.log(arguments);
+// })()
+
+function sum(a, b) {
+  return (a += b);
+}
+
+console.log(sum(6, 5)); //11
+console.log(sum(6, 5)); //11
+console.log(sum(6, 5)); //11
+
+let a = 6;
+function sum2(b) {
+  return (a += b);
+}
+console.log(sum2(5)); //11
+console.log(sum2(5)); //16
+console.log(sum2(5)); //21
