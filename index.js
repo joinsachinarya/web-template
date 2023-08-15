c6471ea6ae44619ff9e1c3f5479d0713750a414f
sac6inarya <sachinkarya333@gmail.com>
2023-08-15 13:27:01 +0530

    example of callback promise and async await

--- index.js
+++ index.js
@@ -1,28 +1,75 @@
+//problem
 function a() {
   console.log("a");
 }
 function b() {
-  console.log("b");
-}
-
-function c() {
   setTimeout(() => {
-    console.log("c");
-    function d() {
-      console.log("d");
-      function e() {
-        console.log("e");
-        function f() {
-          console.log("f");
-        }
-        f();
-      }
-      e();
-    }
-    d();
+    console.log("b");
   }, 2000);
 }
-
+function c() {
+  console.log("c");
+}
 a();
 b();
 c();
+
+// callback solution
+function a() {
+  console.log("a");
+}
+function b(callback) {
+  setTimeout(() => {
+    console.log("b");
+    callback();
+  }, 2000);
+}
+function c() {
+  console.log("c");
+}
+a();
+b(c);
+
+// promise solution
+function a() {
+  console.log("a");
+}
+function b() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("b");
+      resolve();
+    }, 2000);
+  });
+}
+function c() {
+  console.log("c");
+}
+a();
+b().then(() => {
+  c();
+});
+
+// async syntax promise solution
+function a() {
+  console.log("a");
+}
+
+function b() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("b");
+      resolve();
+    }, 2000);
+  });
+}
+
+function c() {
+  console.log("c");
+}
+a();
+async function run() {
+  await b();
+  c();
+}
+run();
