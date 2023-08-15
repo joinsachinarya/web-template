8ec09a0824835ca4dbea3f94f473f5dea5338748
sac6inarya <sachinkarya333@gmail.com>
2023-08-16 00:05:28 +0530

    add nodemon as dev dependencies

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const routes = require("./routes");
 const server = http.createServer(routes);
 
 server.listen(4000, () => {
-  console.log("Server is listening on port 4000");
+  console.log("Server is listening on port 4000!");
 });
