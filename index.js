d81b95416e5d96b83d31c132e899a00520e56ec4
sac6inarya <sachinkarya333@gmail.com>
2023-08-16 09:52:04 +0530

    replace http.createserver with app.listen

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const http = require("http");
-
 const express = require("express");
 const app = express();
 
-const server = http.createServer(app);
-
-server.listen(4000, () => {
+app.listen(4000, () => {
   console.log("Server is listening on port 4000!");
 });
