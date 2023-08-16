5b76b60b3dbda67d075662fce1760e0adc085fe2
sac6inarya <sachinkarya333@gmail.com>
2023-08-17 01:06:25 +0530

    serve css file staticly

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const path = require("path");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use("/shop", shopRoutes);
 app.use(contactRoutes);
