30f74f11a590774fad9f49c5815bce5a3fe40acf
sac6inarya <sachinkarya333@gmail.com>
2023-08-16 09:37:57 +0530

    cleanup

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const http = require("http");
-const routes = require("./routes");
 
-const server = http.createServer(routes);
+const express = require("express");
+const app = express();
+
+const server = http.createServer(app);
 
 server.listen(4000, () => {
   console.log("Server is listening on port 4000!");
