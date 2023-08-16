07ec8760b129e6ad2fb2d0f3a154e1c62d7d5b18
sac6inarya <sachinkarya333@gmail.com>
2023-08-16 09:54:50 +0530

    add middlewares

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("Logging inside a middleware");
+  next();
+});
+app.use((req, res, next) => {
+  console.log("Logging inside anohter middleware");
+});
+
 app.listen(4000, () => {
   console.log("Server is listening on port 4000!");
 });
