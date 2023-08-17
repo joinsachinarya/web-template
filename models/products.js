1c3d3a2aaf7b77c8a648964fa394a4bb1fc4cab1
sac6inarya <sachinkarya333@gmail.com>
2023-08-17 16:22:29 +0530

    store products using fs module

--- models/products.js
+++ models/products.js
@@ -1,12 +1,30 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+const rootDir = require("../util/path");
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
   save() {
-    products.push(this);
+    let p = path.join(rootDir, "data", "products.json");
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.error(err);
+      });
+    });
   }
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    let p = path.join(rootDir, "data", "products.json");
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      }
+      cb(JSON.parse(fileContent));
+    });
   }
 };
