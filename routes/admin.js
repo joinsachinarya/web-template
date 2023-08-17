974b0e6b52756beb7907c93c097962539e18c85e
sac6inarya <sachinkarya333@gmail.com>
2023-08-17 09:43:01 +0530

    add controller for working with data logic

--- routes/admin.js
+++ routes/admin.js
@@ -1,29 +1,13 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const productController = require("../controllers/products");
 
 const router = express.Router();
 
-const products = [];
+router.get("/add-product", productController.getAddProduct);
 
-// /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-});
+router.post("/add-product", productController.postAddProduct);
 
-// /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
