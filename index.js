bbd99fa9a7e3fefee02313682097084c0f814001
sac6inarya <sachinkarya333@gmail.com>
2023-09-16 10:54:04 +0530

    async await

--- index.js
+++ index.js
@@ -1,32 +1,46 @@
-const p = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Promise resolved value!");
-  }, 10000);
-});
+// const p = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Promise resolved value!");
+//   }, 10000);
+// });
 
-const p2 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Promise resolved value 2!");
-  }, 20000);
-});
+// const p2 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Promise resolved value 2!");
+//   }, 20000);
+// });
 
+// async function fun() {
+//   // // JS engine will not wait for promise to be resloved
+//   // console.log("before promise");
+//   // let data = p.then((res) => console.log(res)); // Promise resloved using .then /.catch
+//   // console.log("after promise");
+//   // return data;
+
+//   //JS engine will  suspend the execution until promise is not resolved
+//   console.log("before promise");
+//   let data = await p; // Promise resolved using async, await
+//   console.log(data);
+//   console.log("after promise");
+
+//   //JS engine will  suspend the execution until promise is not resolved
+//   console.log("before promise");
+//   let data2 = await p2; // Promise resolved using async, await
+//   console.log(data2);
+//   console.log("after promise");
+// }
+// fun();
+
+//Example
+const url = "https://api.github.com/users/sac6inarya";
 async function fun() {
-  // // JS engine will not wait for promise to be resloved
-  // console.log("before promise");
-  // let data = p.then((res) => console.log(res)); // Promise resloved using .then /.catch
-  // console.log("after promise");
-  // return data;
-
-  //JS engine will  suspend the execution until promise is not resolved
-  console.log("before promise");
-  let data = await p; // Promise resolved using async, await
-  console.log(data);
-  console.log("after promise");
-
-  //JS engine will  suspend the execution until promise is not resolved
-  console.log("before promise");
-  let data2 = await p2; // Promise resolved using async, await
-  console.log(data2);
-  console.log("after promise");
+  // fetch(url).then((res) => res.json().then((result) => console.log(result)).catch(err=>console.error(err)));
+  try {
+    let data = await fetch(url);
+    let jsonValue = await data.json();
+    console.log(jsonValue);
+  } catch (error) {
+    console.error(error);
+  }
 }
 fun();
