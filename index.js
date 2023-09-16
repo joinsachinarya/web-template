5fafe132184d3b1e0fe06f79b9fc317c3f657ce9
sac6inarya <sachinkarya333@gmail.com>
2023-09-16 09:41:39 +0530

    await keyword make promise to wait unitll it resolves

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const p = new Promise((resolve, reject) => {
 
 async function fun() {
   // JS engine will not wait for promise to be resloved
+  console.log("before promise");
   let data = p.then((res) => console.log(res)); // Promise resloved using .then /.catch
-  console.log("Hye");
+  console.log("after promise");
   return data;
 
   // //JS engine will wait for promise to be resolved
+  // console.log("before promise");
   // let data = await p; // Promise resolved using async, await
-  // console.log("HYE");
   // console.log(data);
+  // console.log("after promise");
 }
 fun();
