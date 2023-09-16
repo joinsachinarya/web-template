e2cb250eb701c693e6aba327626c33b241423b6c
sac6inarya <sachinkarya333@gmail.com>
2023-09-16 08:42:47 +0530

    ansync keyword make a function asynchronous and return promise

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Logging!");
+const p = new Promise((resolve, reject) => {
+  resolve("Promise resolved value!");
+});
+
+async function fun() {
+  // return p;
+  return "Direct value!";
+}
+let data = fun();
+
+data.then((result) => {
+  console.log(result);
+});
