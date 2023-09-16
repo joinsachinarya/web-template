9a099747e303f4337ab3704d9f66254c133bb241
sac6inarya <sachinkarya333@gmail.com>
2023-09-16 10:19:25 +0530

    await keyword make function to suspend unitl promise is not resloved

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 const p = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve("Promise resolved value!");
-  }, 2000);
+  }, 10000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Promise resolved value 2!");
+  }, 20000);
 });
 
 async function fun() {
-  // JS engine will not wait for promise to be resloved
-  console.log("before promise");
-  let data = p.then((res) => console.log(res)); // Promise resloved using .then /.catch
-  console.log("after promise");
-  return data;
-
-  // //JS engine will wait for promise to be resolved
+  // // JS engine will not wait for promise to be resloved
   // console.log("before promise");
-  // let data = await p; // Promise resolved using async, await
-  // console.log(data);
+  // let data = p.then((res) => console.log(res)); // Promise resloved using .then /.catch
   // console.log("after promise");
+  // return data;
+
+  //JS engine will  suspend the execution until promise is not resolved
+  console.log("before promise");
+  let data = await p; // Promise resolved using async, await
+  console.log(data);
+  console.log("after promise");
+
+  //JS engine will  suspend the execution until promise is not resolved
+  console.log("before promise");
+  let data2 = await p2; // Promise resolved using async, await
+  console.log(data2);
+  console.log("after promise");
 }
 fun();
