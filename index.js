40bd1d120b7faf40f63c1e8f93c9f120e5ae675f
Sachin Arya <sachin.arya@preplaced.in>
2023-10-04 00:57:53 +0530

    we can tarce event bubbeling using e.currentTarget.tagName object

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const output = document.querySelector("#output");
 const container = document.querySelector("#container");
 const handleClick = (e) => {
-    output.textContent = `${e.target.tagName} element was clicked!`
+    output.textContent += `${e.currentTarget.tagName} element was clicked!`
 }
 output.addEventListener("click", handleClick);
+
+document.body.addEventListener("click", handleClick);
+document.querySelector("button").addEventListener("click", handleClick)
 container.addEventListener("click", handleClick)
