2955fece949b5d0234911725f81fd58e5893c586
Sachin Arya <sachin.arya@preplaced.in>
2023-10-04 00:47:50 +0530

    normal event

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hey!");
+const output = document.querySelector("#output");
+const container = document.querySelector("#container");
+const handleClick = (e) => {
+    output.textContent = `${e.target.tagName} element was clicked!`
+}
+output.addEventListener("click", handleClick);
+container.addEventListener("click", handleClick)
